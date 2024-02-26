49425aa5142d4ba732892fe36d01c1e1eb6dbec7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-02-27 05:59:03 +0900

    💄 create page navigation templete

---
+++ react-gakujo/src/components/FixedBottomNavigation.tsx
@@ -0,0 +1,43 @@
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import RestoreIcon from '@mui/icons-material/Restore';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import Hoge1 from './Hoge1';
+import Hoge2 from './Hoge2';
+import Hoge3 from './Hoge3';
+
+export default function FixedBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+      <CssBaseline />
+      {contents[value]}
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
+
+const contents = [
+  <Hoge1 />,
+  <Hoge2 />,
+  <Hoge3 />,
+];
---
+++ react-gakujo/src/components/Hoge1.tsx
@@ -0,0 +1,9 @@
+function Hoge1() {
+  return (
+    <div>
+        <h1>Hoge1</h1>
+    </div>
+  );
+}
+
+export default Hoge1;
