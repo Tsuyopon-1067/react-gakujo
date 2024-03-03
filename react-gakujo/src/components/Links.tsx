4f7d9f24db33fbf528c85fb4a25c2bb677d8510b
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-04 01:04:35 +0900

    💄 make icon react component from svg

--- react-gakujo/src/components/Links.tsx
+++ react-gakujo/src/components/Links.tsx
@@ -1,7 +1,20 @@
 import { MenuBook } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { CSSProperties } from "react";
 import styles from "./Links.module.css";
-import gakujoLogo from "/gakujo.svg";
+
+interface PropsType {
+    style?: CSSProperties;
+    color?: string;
+}
+
+const GakujoIcon = (props: PropsType) => {
+    return (
+        <svg width="37.078" height="30.295" viewBox="0 0 9.81 8.016" xmlns="http://www.w3.org/2000/svg" style={props.style ? props.style : undefined}>
+            <path d="M.138 6.39h-5.276v5.276H.138zm-2.246 6.851-3.954 3.954.48.48 3.954-3.954zm-.76-.348h-3.498V9.49l-2.914 2.914v3.403h3.497z" transform="scale(1.27979 .60179) rotate(-45 -5.779 -1.142)" />
+        </svg>
+    )
+}
 
 
 function Links() {
@@ -9,7 +22,7 @@ function Links() {
         <div className={styles.main_div}>
             <Button variant="contained" fullWidth sx={{ backgroundColor: "#04CBB0" }} href="https://gakujo.shizuoka.ac.jp/portal/">
                 <div className={styles.button_div}>
-                    <img src={gakujoLogo} width={200} style={{ marginTop: 20 }} />
+                    <GakujoIcon style={{ fill: "#00000088", width: 200, height: 200, marginTop: 20, marginBottom: -20 }} />
                     <p className={styles.caption}>
                         学務情報<br />システム
                     </p>
