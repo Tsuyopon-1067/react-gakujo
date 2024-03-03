5bfacad694400af1f5ccfd9588941b15cb682ed3
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-03 22:23:36 +0900

    💄 allow to apply color to link button

--- react-gakujo/src/components/Links.tsx
+++ react-gakujo/src/components/Links.tsx
@@ -1,13 +1,16 @@
 import { MenuBook } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { MainLocalStorageData } from "./FixedBottomNavigation";
 import styles from "./Links.module.css";
 import gakujoLogo from "/gakujo.svg";
 
 
 function Links() {
+    const primaryColor = MainLocalStorageData.getColor().getPrimaryColor();
+    const primaryColorHover = MainLocalStorageData.getColor().getPrimaryColorHover();
     return (
         <div className={styles.main_div}>
-            <Button variant="contained" fullWidth sx={{ backgroundColor: "#04CBB0" }} href="https://gakujo.shizuoka.ac.jp/portal/">
+            <Button variant="contained" fullWidth sx={{ backgroundColor: primaryColor, ":hover": { background: primaryColorHover } }} href="https://gakujo.shizuoka.ac.jp/portal/">
                 <div className={styles.button_div}>
                     <img src={gakujoLogo} width={200} style={{ marginTop: 20 }} />
                     <p className={styles.caption}>
@@ -17,7 +20,7 @@ function Links() {
             </Button>
             <br />
             <br />
-            <Button variant="contained" fullWidth sx={{ backgroundColor: "#04CBB0" }} href="https://opac.lib.shizuoka.ac.jp/simple/">
+            <Button variant="contained" fullWidth sx={{ backgroundColor: primaryColor, ":hover": { background: primaryColorHover } }} href="https://opac.lib.shizuoka.ac.jp/simple/">
                 <div className={styles.button_div}>
                     <MenuBook sx={{ fontSize: 200 }} color="action" />
                     <p className={styles.caption}>
