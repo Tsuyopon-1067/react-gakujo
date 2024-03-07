528130cdd357cc08cca8a9c00b82e709cd2017b7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-07 09:12:09 +0900

    🚧 fetch each depart time

--- react-gakujo/src/components/fetchBusStop/fetchBusStop.go
+++ react-gakujo/src/components/fetchBusStop/fetchBusStop.go
@@ -8,30 +8,43 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Bus struct {
+	Departure string
+	route string
+}
+
 func main() {
 	url1 := "https://info.entetsu.co.jp/navi/pc/dispjikokutable.aspx?tp=HTML&bs=290020&pn=1&nw=0"
 	//url2 := "https://info.entetsu.co.jp/navi/pc/dispjikokutable.aspx?tp=HTML&bs=290020&pn=5&nw=0"
+	data1 := []Bus{}
+	fetchTimeTable(url1, data1)
 
+}
+
+func fetchTimeTable(url string, data []Bus) {
 	c := colly.NewCollector()
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
-
 		for i := 3; i < 24; i++ {
-			fmt.Printf("%d:", i)
-			for j := 1; j < 20; j++ {
-				path := fmt.Sprintf("body > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(%d) > td:nth-child(%d)", i, j)
-				text := e.DOM.Find(path).Text()
-				text = strings.Replace(text, " ", "", -1)
-				text = strings.Replace(text, "\t", "", -1)
-				text = strings.Replace(text, "\n", "", -1)
-				if text == "" {
-					break
-				}
-				fmt.Printf("%d:%s, ", j, text)
+			for j := 1; j <= 3; j++ {
+				fetchHour(i, j, e)
 			}
-			fmt.Println()
 		}
 	})
 
-	c.Visit(url1)
+	c.Visit(url)
+}
+
+func fetchHour(row int, col int, e *colly.HTMLElement) {
+	for i := 1; i <= 20; i++ {
+		path := fmt.Sprintf("body > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(%d) > td:nth-child(%d) > a:nth-child(%d)", row, col, i)
+		text := e.DOM.Find(path).Text()
+		text = strings.Replace(text, " ", "", -1)
+		text = strings.Replace(text, "\t", "", -1)
+		text = strings.Replace(text, "\n", "", -1)
+		if text == "" {
+			return
+		}
+		fmt.Printf("%d,%d,%d: %s, \n", row, col, i, text)
+	}
 }
