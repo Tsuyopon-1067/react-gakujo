623594c693eaa86d4bc4f725dd9c9bd8266b49ac
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-07 09:19:26 +0900

    ♻️ create function to remove spaces

--- react-gakujo/src/components/fetchBusStop/fetchBusStop.go
+++ react-gakujo/src/components/fetchBusStop/fetchBusStop.go
@@ -26,7 +26,11 @@ func fetchTimeTable(url string, data []Bus) {
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		for i := 3; i < 24; i++ {
-			for j := 1; j <= 3; j++ {
+			path := fmt.Sprintf("body > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(%d) > td:nth-child(%d)", i, 1)
+			text := e.DOM.Find(path).Text()
+			text = removeSpace(text)
+			fmt.Printf("[%s]\n", text)
+			for j := 2; j <= 3; j++ {
 				fetchHour(i, j, e)
 			}
 		}
@@ -39,12 +43,17 @@ func fetchHour(row int, col int, e *colly.HTMLElement) {
 	for i := 1; i <= 20; i++ {
 		path := fmt.Sprintf("body > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(%d) > td:nth-child(%d) > a:nth-child(%d)", row, col, i)
 		text := e.DOM.Find(path).Text()
-		text = strings.Replace(text, " ", "", -1)
-		text = strings.Replace(text, "\t", "", -1)
-		text = strings.Replace(text, "\n", "", -1)
+		text = removeSpace(text)
 		if text == "" {
 			return
 		}
 		fmt.Printf("%d,%d,%d: %s, \n", row, col, i, text)
 	}
+}
+
+func removeSpace(text string) string {
+	text = strings.Replace(text, " ", "", -1)
+	text = strings.Replace(text, "\t", "", -1)
+	text = strings.Replace(text, "\n", "", -1)
+	return text
 }
