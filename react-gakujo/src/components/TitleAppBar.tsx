9c326e35a694ad1080524db73862373e3b7be71d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-08 18:00:52 +0900

    💄 add back button on title bar

--- react-gakujo/src/components/TitleAppBar.tsx
+++ react-gakujo/src/components/TitleAppBar.tsx
@@ -1,4 +1,4 @@
-import { Settings } from "@mui/icons-material";
+import { ArrowBack, ArrowLeft, Settings } from "@mui/icons-material";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,12 +8,16 @@ interface TitleAppBarProps {
     title: string;
     primaryColor: string;
     fontColor: string;
+    handleBackButton: () => void;
+    isSubPage: boolean;
 }
 
 export default function TitleAppBar({
     title,
     primaryColor,
     fontColor,
+    handleBackButton,
+    isSubPage,
 }: TitleAppBarProps) {
     return (
         <AppBar
@@ -21,6 +25,11 @@ export default function TitleAppBar({
             sx={{ color: fontColor, background: primaryColor }}
         >
             <Toolbar>
+                {isSubPage && (
+                    <IconButton onClick={() => handleBackButton()}>
+                        <ArrowBack style={{ color: fontColor }} />
+                    </IconButton>
+                )}
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     {title}
                 </Typography>
