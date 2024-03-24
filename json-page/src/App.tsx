70b2cf058cc5bd225c783aadb766e6d52534ff18
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-24 16:31:38 +0900

    💄 add bi and ia department button

--- json-page/src/App.tsx
+++ json-page/src/App.tsx
@@ -1,6 +1,10 @@
 import { Grid } from "@mui/material";
 import styles from "./App.module.css";
-import { InfJsonDataList } from "./components/JsonButtonPropsData";
+import {
+    InfBiJsonDataList,
+    InfCsJsonDataList,
+    InfIaJsonDataList,
+} from "./components/JsonButtonPropsData";
 import JsonButtons from "./components/JsonButtons";
 import SimpleAppBar from "./components/SimpleAppBar";
 
@@ -12,7 +16,19 @@ function App() {
                 <h1 className={styles.h1}>情報学部</h1>
                 <h2 className={styles.h2}>情報科学科</h2>
                 <Grid container spacing={2} m={1} width={"100%"}>
-                    {InfJsonDataList.map((data, index) => (
+                    {InfCsJsonDataList.map((data, index) => (
+                        <JsonButtons key={index} data={data} />
+                    ))}
+                </Grid>
+                <h2 className={styles.h2}>行動情報学科</h2>
+                <Grid container spacing={2} m={1} width={"100%"}>
+                    {InfBiJsonDataList.map((data, index) => (
+                        <JsonButtons key={index} data={data} />
+                    ))}
+                </Grid>
+                <h2 className={styles.h2}>情報社会学科</h2>
+                <Grid container spacing={2} m={1} width={"100%"}>
+                    {InfIaJsonDataList.map((data, index) => (
                         <JsonButtons key={index} data={data} />
                     ))}
                 </Grid>
