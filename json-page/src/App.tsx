4bca8acba17e837d5a788d1773507e984b516f07
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-04-12 09:36:02 +0900

    ✨ add json data

--- json-page/src/App.tsx
+++ json-page/src/App.tsx
@@ -1,40 +1,48 @@
 import { Grid } from "@mui/material";
 import styles from "./App.module.css";
 import {
-    InfBiJsonDataList,
-    InfCsJsonDataList,
-    InfIaJsonDataList,
+  EnEJsonDataList,
+  InfBiJsonDataList,
+  InfCsJsonDataList,
+  InfIaJsonDataList,
 } from "./components/JsonButtonPropsData";
 import JsonButtons from "./components/JsonButtons";
 import SimpleAppBar from "./components/SimpleAppBar";
 
 function App() {
-    return (
-        <>
-            <SimpleAppBar />
-            <div className={styles.main_div}>
-                <h1 className={styles.h1}>情報学部</h1>
-                <h2 className={styles.h2}>情報科学科</h2>
-                <Grid container spacing={2} m={1} width={"100%"}>
-                    {InfCsJsonDataList.map((data, index) => (
-                        <JsonButtons key={index} data={data} />
-                    ))}
-                </Grid>
-                <h2 className={styles.h2}>行動情報学科</h2>
-                <Grid container spacing={2} m={1} width={"100%"}>
-                    {InfBiJsonDataList.map((data, index) => (
-                        <JsonButtons key={index} data={data} />
-                    ))}
-                </Grid>
-                <h2 className={styles.h2}>情報社会学科</h2>
-                <Grid container spacing={2} m={1} width={"100%"}>
-                    {InfIaJsonDataList.map((data, index) => (
-                        <JsonButtons key={index} data={data} />
-                    ))}
-                </Grid>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <SimpleAppBar />
+      <div className={styles.main_div}>
+        <h1 className={styles.h1}>情報学部</h1>
+        <h2 className={styles.h2}>情報科学科</h2>
+        <Grid container spacing={2} m={1} width={"100%"}>
+          {InfCsJsonDataList.map((data, index) => (
+            <JsonButtons key={index} data={data} />
+          ))}
+        </Grid>
+        <h2 className={styles.h2}>行動情報学科</h2>
+        <Grid container spacing={2} m={1} width={"100%"}>
+          {InfBiJsonDataList.map((data, index) => (
+            <JsonButtons key={index} data={data} />
+          ))}
+        </Grid>
+        <h2 className={styles.h2}>情報社会学科</h2>
+        <Grid container spacing={2} m={1} width={"100%"}>
+          {InfIaJsonDataList.map((data, index) => (
+            <JsonButtons key={index} data={data} />
+          ))}
+        </Grid>
+        <h1 className={styles.h1}>工学部</h1>
+        <h2 className={styles.h2}>電気電子工学科</h2>
+        <Grid container spacing={2} m={1} width={"100%"}>
+          {EnEJsonDataList.map((data, index) => (
+            <JsonButtons key={index} data={data} />
+          ))}
+        </Grid>
+      </div>
+    </>
+  );
 }
 
 export default App;
